7e5fc7a08296c6f14877a7a1c90cb27271582165
a_hirayama <a_hirayama@ngo.ohara.ac.jp>
2025-11-30 18:30:17 +0900

    11/30学習分

--- JavaScript2/code/6chap/04-demo/js/script.js
+++ JavaScript2/code/6chap/04-demo/js/script.js
@@ -1,8 +1,19 @@
 const heading = document.querySelector('#heading');
+const rote = document.querySelector('#rote');
+const color = document.querySelector('#color');
 
 const keyframes = {
+    color: ['transparent', '#fff'],
+    backgroundPosition: ['100% 0', '0 0'],
+};
+
+const keyframes2 = {
     opacity: [0, 1],
-    translate: ['0 100px', '200px 300px'],
+    rotate: ['y 360deg', 0],
+};
+
+const keyframes3 = {
+    color: ['#f66', '#fc2', '#0c9', '#0bd']
 };
 
 const option = {
@@ -10,7 +21,15 @@ const option = {
     easing: 'ease'
 }
 
+const option2 = {
+    duration: 8000,
+    direction: 'alternate',
+    iterations: Infinity,
+}
+
 heading.animate(keyframes, option)
+rote.animate(keyframes2, option)
+color.animate(keyframes3, option2)
 
 
 // heading.style.transform = 'translate(100px, 300px) rotate(15deg) scale(2.5)';
