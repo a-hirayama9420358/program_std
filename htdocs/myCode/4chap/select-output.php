5fbbdd4b641af22b80b96b08e1428d230aad3011
a_hirayama <a_hirayama@ngo.ohara.ac.jp>
2026-02-08 17:57:04 +0900

    2/8

---
+++ htdocs/myCode/4chap/select-output.php
@@ -0,0 +1,15 @@
+<?php require '../header.php'; ?>
+<?php
+switch($_REQUEST["seat"]){
+    case "指定席":
+        echo "追加料金は１２００円です";
+        break;
+    case "グリーン席":
+        echo "追加料金は２５００円です";
+        break;
+    default:
+        echo "追加料金は不要です";
+        break;
+}
+?>
+<?php require '../footer.php'; ?>
