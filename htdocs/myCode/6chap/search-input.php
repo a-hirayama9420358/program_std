c557300f4fe0ccdc6b0569c9daa7d9cd5456a351
a_hirayama <a_hirayama@ngo.ohara.ac.jp>
2026-03-29 15:56:30 +0900

    3/29

---
+++ htdocs/myCode/6chap/search-input.php
@@ -0,0 +1,9 @@
+<?php require '../header.php'; ?>
+商品名を入力して下さい
+
+<form action='search-output.php' method='post'>
+    <input type='text' name='keyword'>
+    <input type='submit' value='ここで検索'>
+</form>
+
+<?php require '../footer.php'; ?>
---
+++ htdocs/myCode/6chap/search-output.php
@@ -0,0 +1,23 @@
+<?php require '../header.php'; ?>
+<table>
+    <tr>
+        <th>商品番号</th>
+        <th>商品名</th>
+        <th>価格</th>
+    </tr>
+
+<?php
+$pdo = new PDO('mysql:host=localhost;dbname=shop;charset=utf8','staff','password');
+$sql = $pdo->prepare('select * from product where name=?');
+$sql->execute([$_REQUEST['keyword']]);
+foreach($sql as $row){
+    echo "<tr>
+            <td>$row[id]</td>
+            <td>$row[name]</td>
+            <td>$row[price]</td>
+            </tr>";
+}
+?>
+</table>
+
+<?php require '../footer.php'; ?>
